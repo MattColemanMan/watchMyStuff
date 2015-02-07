82fd4cfba7f8e239bd80b1ca5de170b5bdebd7d7
RayOconnor <roconnorc@gmail.com>
2015-02-07 03:09:56 -0500

    created user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Myapp::Application.routes.draw do
+  resources :users
+
   get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
