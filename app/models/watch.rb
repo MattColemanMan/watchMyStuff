caf4582f182ede5d41cb5d7fad083b3c5259044a
Matthew Coleman <kirocuto@gmail.com>
2015-02-08 07:45:53 -0500

    watch model

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -2,10 +2,8 @@ class Watch < ActiveRecord::Base
 	belongs_to :user
 	after_save { if !self.watcher_id 
 					self.update_column(:status, "Unfulfilled Watch")
-				 elsif self.watcher_id and !self.watcher_ready
+				 elsif self.watcher_id and !self.start_time
 				 	self.update_column(:status, "Accepted Watch")
-				 elsif self.watcher_ready and !self.start_time
-				 	self.update_column(:status, "Watcher Arrived")
 				 elsif self.start_time and !self.end_time
 				 	self.update_column(:status, "In-progress Watch")
 				 else
